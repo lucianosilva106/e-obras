93c79853e9755a81ef6e1ed98825c8b3110c1300
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-08-10 11:26:12 -0300

    removido tudo tudo

--- src/pages/Formulario/index.js
+++ src/pages/Formulario/index.js
@@ -1,15 +1,5 @@
-import React, { useState } from "react";
-
-import "./styles.css";
 
 function Form() {
-  const [name, setName] = useState('');
-  const [last_name, setLastName] = useState('');
-  const [cpf, setCpf] = useState('');
-  const [news, setNews] = useState(true);
-  const [promotions, setPromotions] = useState(true);
-
-  const [CPFError, setCPFError] = useState(false);
 
   return (
     <div>Preencha o formulario</div>
