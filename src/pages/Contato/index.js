2d1a13aa8d9cf5e3036bab54e163d7db816d403a
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-08-11 14:55:05 -0300

    site navegando sem erros

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,8 +1,47 @@
+import React, { useState } from 'react'
+import './Contato.css'
+
 function Contato(){
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [mensagem, setMensagem] = useState('');
+
+    function handleEnvia(){
+        alert('A Konezia agradece seu contato')
+      }
+      
     return(
-        <div>        
+        <div className="texto">        
             <h2>Olá, meu nome é Benedito Caetano e vou te acompanhar durante toda a solução do seu problema.</h2> <p/>
             <h3>Meu e-mail é bene.caetano@uol.com.br e meu celular e whatsapp é ddd 11 99123 4567</h3>
+            <hr/>
+            <h3>Deixe sua mensagem</h3>
+            <form>
+                <label>Nome Completo:
+                  <input 
+                    type="text" 
+                    value={name}
+                    onChange={(e) => setName(e.target.name)}
+                  />
+                </label> <br/>
+                <label>Email:
+                  <input 
+                    type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.email)}
+                  />
+                </label> <br/>
+                <textarea name="textarea"
+                  value={mensagem}
+                  onChange={(e) => setMensagem(e.target.value)}>
+                    Mensagem:
+
+                </textarea> <br/>
+                <button onChange={() => handleEnvia()}> Enviar Mensagem</button>
+            </form>
+
+
         </div>
     );
 }
--- src/pages/Formulario/index.js
+++ src/pages/Formulario/index.js
@@ -1,24 +1,50 @@
 import React, { useState } from 'react';
 
+import './Formulario.css'
+
 function Form() {
 
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [habilidade, setHabilidade] = useState('');
 
   function handleGrava(){
     alert('vou gravar')
   }
 
   return (
-    <div>
+    <div className='texto'>
       <br/>
       <form>
-        <label>Nome:
+        <label>Nome Completo:
           <input 
             type="text" 
             value={name}
-            onChange={(e) => setName(e.target.name)}
-          />
+            onChange={(e) => setName(e.target.value)}
+          /> <br/>
        </label>
+       <label>Email:
+          <input 
+            type="text" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+       </label><br/>
+       <label>Celular/Whatsapp:
+          <input 
+            type="text" 
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+          />
+       </label><br/>
+       <label>Habilidades profissionais:
+          <input 
+            type="text" 
+            value={habilidade}
+            onChange={(e) => setHabilidade(e.target.value)}
+          />
+       </label><br/>
         <button onChange={() => handleGrava()}> Enviar Formulário</button>
       </form>
     </div>
