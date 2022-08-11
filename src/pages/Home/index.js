72d44e4476ce64829b99480920d6d81b1d39c5d2
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-08-11 14:29:00 -0300

    inserçao de imagens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import construir from '../img/construcao.jpg'
+import reformar from '../img/reforma.jpg'
 import './home.css';
 
 function Home(){
@@ -5,13 +8,14 @@ function Home(){
     return(
         <div>
             <div id="logohome" />
-            <div>
-                <h2 style={{color: '#brown', marginTop: '8cm'}}>Quer construir?</h2>
+            <div className='texto'>
+                <h2>Quer construir?</h2>
                 <h3>Você que deseja iniciar uma obra do zero é importante um planejamento minucioso para que o cronograma
                     de sua obra saia dentro do prazo definido e do orçamento calculado, gerando economia e agilidade ao empreendimento.</h3> <br/>
-                <h2 style={{color: '#brown'}}>Quer reformar?</h2>
-                <h3> Uma reforma requer cuidados estruturais e muita criatividade e competência para otimizar espaços, evitar desperdício de tempo e dinheiro.</h3>
-                <hr/>
+                <img src={construir} alt='Construções' className='imagens'/>
+                <h2>Quer reformar?</h2>
+                <h3> Uma reforma requer cuidados estruturais e muita criatividade e competência para otimizar espaços, evitar desperdício de tempo e dinheiro.</h3> <br/>
+                <img src={reformar} alt='Reformas' className='imagens'/>
                 <div className='textofoot'>
                     <a style={{color: 'cadetblue'}} href='https://www.google.com.br' target='_blank' rel='noreferrer'> Google</a> 
                     <a style={{color: 'cadetblue'}} href='https://www.instagram.com' target='_blank' rel='noreferrer'> Instagran</a> 
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,6 +1,20 @@
+import './Sobre.css';
+
 function Sobre(){
     return(
-        <div>Sobre a pagina</div>
+        <div className='texto'>
+            <h3>A Konezia projeto & construção é uma empresa de prestação de serviços na área de construção civil.</h3>
+            <h3>Há mais de 10 anos no mercado a Konezia entrega qualidade, rapidez e segurança em suas obras.</h3>
+            <br/>
+            <h2>Missão, Visão e Valores</h2>
+            <hr/>
+            <h3>Missão</h3>
+            <h3>Trabalhar com honestidade, segurança e transparência.</h3>
+            <h3>Visão</h3>
+            <h3>Garantir qualidade ao cliente no preço e tempo desejado.</h3>
+            <h3>Valores</h3>
+            <h3>Trabalhar com eficiência sem desperdício de tempo e dinheiro com a máxima preocupação com o meio ambiente e descartes de resíduos.</h3>
+        </div>
     );
 }
 
